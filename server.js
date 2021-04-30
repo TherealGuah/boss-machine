b746a14459f6ad3101acdec7a8c1c927c35c57db
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-04-30 23:40:56 +0100

    added log of port being listen for

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use('/api', apiRouter);
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-  app.listen(PORT);
+  app.listen(PORT, () => console.log('Server listening on http://localhost:4001'));
 }
