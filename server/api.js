d1c2f6a53eea652bf049ad7e2ff2b3f041f2b0fd
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-04-30 23:38:25 +0100

    added routing to server.js and created basic layout for routes in api.js

--- server/api.js
+++ server/api.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+import { createMeeting } from './db';
 
+/*      '/api/minions'      */
+
+// get all minions
+
+// post new minion
+
+// get single minion by Id
+
+// update minion by Id
+
+// delete minion by Id
+
+/*      '/api/ideas'        */
+
+// get all ideas
+
+// create new idea
+
+// get single idea by Id
+
+// update idea by Id
+
+// delete idead by Id
+
+/*      '/api/meetings'        */
+
+// get all meetings
+
+// create new meeting
+
+// delete all meetings
 
 module.exports = apiRouter;
