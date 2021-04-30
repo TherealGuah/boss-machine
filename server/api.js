07d1fb20891a53eeeb354c59d923aa3e7dca5710
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-05-01 00:03:15 +0100

    created basic routes. Missing handlers for each

--- server/api.js
+++ server/api.js
@@ -1,38 +1,42 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
 
-import { createMeeting } from './db';
+const createMeeting = require('./db');
 
 /*      '/api/minions'      */
 
 // get all minions
-
+app.get('/minions', );
 // post new minion
-
+app.post('/minions', );
 // get single minion by Id
-
+app.get('minions/:Id', );
 // update minion by Id
-
+app.put('minions/:Id', );
 // delete minion by Id
+app.delete('minions/:Id', );
+
 
 /*      '/api/ideas'        */
 
 // get all ideas
-
+app.get('/ideas', );
 // create new idea
-
+app.post('/ideas', );
 // get single idea by Id
-
+app.get('/ideas/:Id', );
 // update idea by Id
-
+app.put('/ideas/:Id', );
 // delete idead by Id
+app.delete('/ideas/:Id', );
 
 /*      '/api/meetings'        */
 
 // get all meetings
-
+app.get('/meetings', );
 // create new meeting
-
+app.post('/meetings', );
 // delete all meetings
-
+app.delete('/meetings', );
 module.exports = apiRouter;
