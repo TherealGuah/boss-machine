cb89673903595524ebd718a91f8716f4e0f05332
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-05-03 22:35:54 +0100

    get all and post added but still not passed test

--- server/worksRouter.js
+++ server/worksRouter.js
@@ -14,15 +14,17 @@ const {
 /* '/api/minions/:minionId/work */
 
 worksRouter.get('/', (req, res, next) => {
-
-});
-
-worksRouter.post('/', (req, res, next) => {
-
+    const allWorks = getAllFromDatabase('works');
+    res.status(200).send(allWorks);
 });
 
 worksRouter.put('/:workId', (req, res, next) => {
 
+});
+
+worksRouter.post('/', (req, res, next) => {
+    const newWork = addToDatabase('works', req.body);
+    res.status(201).send(newWork);
 });
 
 worksRouter.delete('/:workId', (req, res, next) => {
