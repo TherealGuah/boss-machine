199dca5ac523584e2d54d6bb29632e4e97c6115e
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-05-03 19:48:19 +0100

    meetings routes completed and tests passed

--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -12,19 +12,30 @@ const {
 } = require('./db');
 
 /*      '/api/meetings'        */
-
+meetingsRouter.use('/', (req, res, next) => {
+    const allMeetings = getAllFromDatabase('meetings');
+    req.allMeetings = allMeetings;
+    next();
+});
 // get all meetings
 meetingsRouter.get('/', (req, res, next) => {
-    const allMeetings = getAllFromDatabase('meetings');
-    res.status(200).send(allMeetings);
+    res.status(200).send(req.allMeetings);
 });
 // create new meeting
 meetingsRouter.post('/', (req, res, next) => {
-
+    const newMeeting = createMeeting();
+    req.allMeetings.push(newMeeting);
+    res.status(201).send(newMeeting);
 });
 // delete all meetings
 meetingsRouter.delete('/', (req, res, next) => {
-
+    const deleted = deleteAllFromDatabase('meetings');
+    if (!deleted) {
+        res.status(500);
+    } else {
+        res.status(204);
+    }
+    res.send();
 });
 
 module.exports = meetingsRouter;
